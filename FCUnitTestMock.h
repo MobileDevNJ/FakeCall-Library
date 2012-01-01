7596770dc246fa34db19f4c6995a8122bc490fe0
Gary R <gary@kaliware.com>
2012-01-01 18:25:13 -0500

    Add Mock Unit Tests, additional error testing

---
+++ FCUnitTestMock.h
@@ -0,0 +1,13 @@
+//
+//  FCUnitTestMock.h
+//  fclibrary
+//
+//  Created by Ainsley Rattray on 1/1/12.
+//  Copyright (c) 2012 Kaliware, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FCUnitTestMock : NSObject
+
+@end
---
+++ FCUnitTestMock.m
@@ -0,0 +1,13 @@
+//
+//  FCUnitTestMock.m
+//  fclibrary
+//
+//  Created by Ainsley Rattray on 1/1/12.
+//  Copyright (c) 2012 Kaliware, LLC. All rights reserved.
+//
+
+#import "FCUnitTestMock.h"
+
+@implementation FCUnitTestMock
+
+@end
